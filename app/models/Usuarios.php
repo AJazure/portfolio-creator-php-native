46614f4c13df59ddfbc0580390d9341fd40da91b
CristianMene <143648242+CristianMene@users.noreply.github.com>
2024-02-28 00:22:15 -0300

    modelo Usuarios, con funcion UPDATE

--- app/models/Usuarios.php
+++ app/models/Usuarios.php
@@ -33,11 +33,22 @@
             $this->db->bind(':mail', $data['mail']);
             $this->db->bind(':password', $data['password']);
             
-            if ($this->db->execute()) {
-                return true;
-            } else {
-                return false;            
-            }
+            // if ($this->db->execute()) {
+            //     return true;
+            // } else {
+            //     return false;            
+            // }
+        }
+
+        public function actualizarDatosUsuario($data){
+            
+            $this->db->query("UPDATE usuarios SET nombre = :nombre, apellido = :apellido, mail = :mail, password = :password WHERE id = :id");
+            $this->db->bind(':id', $data['id']);
+            $this->db->bind(':nombre', $data['nombre']);
+            $this->db->bind(':apellido', $data['apellido']);
+            $this->db->bind(':mail', $data['mail']);
+            $this->db->bind(':password', $data['password']);
+            
         }
         
 
